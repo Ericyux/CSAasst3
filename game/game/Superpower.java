9086cbf114aba1fe406c7bf80019e5734bfdfce3
ranoodles <115419765+ranoodles@users.noreply.github.com>
2023-08-06 15:39:07 -0700

    ur mom

---
+++ game/game/Superpower.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Superpower here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Superpower extends Actor
+{
+    /**
+     * Act - do whatever the Superpower wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
